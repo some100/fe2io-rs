3cf6122bdf6585e619d0d3849b78d27229a4f10b
someone <40119497+some100@users.noreply.github.com>
2025-02-24 14:36:49 +0000

    Add max attempts to retries

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ use rodio::{Sink, Decoder, OutputStream};
 use serde::Deserialize;
 use tungstenite::{WebSocket, connect, Message};
 use tungstenite::stream::MaybeTlsStream;
-use reqwest::blocking::get;
-use anyhow::{Context, Result};
+use anyhow::{Context, anyhow, Result};
 use log::{debug, info, warn, error};
-use env_logger::Env;
 
 /// Lighterweight alternative for fe2.io
 #[derive(Parser)]
@@ -28,6 +26,9 @@ struct Args {
     /// Multiplier for delay in failed connection
     #[arg(long, default_value_t = 2)]
     backoff: u64,
+    /// Amount of times allowed to reconnect to server
+    #[arg(long, default_value_t = 5)]
+    attempts: u64,
 }
 
 #[derive(Deserialize)]
@@ -39,20 +40,38 @@ struct Msg {
 }
 
 fn main() -> Result<()> {
-    let env = Env::default().filter_or("LOG_LEVEL", "info");
+    let env = env_logger::Env::default().filter_or("LOG_LEVEL", "info");
     env_logger::init_from_env(env);
 
     let args = Args::parse();
-    let mut server = connect_to_server(&args.url, &args.username, args.delay, args.backoff)?;
+    let mut server = match connect_to_server(&args.url, &args.username, args.delay, args.backoff, args.attempts) {
+        Ok(server) => server,
+        Err(e) => {
+            error!("{}", e);
+            return Ok(());
+        },
+    };
 
-    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let (_stream, stream_handle) = match OutputStream::try_default() {
+        Ok(stream) => stream,
+        Err(_e) => {
+            error!("Failed to open audio device, most likely because none are available");
+            return Ok(());
+        },
+    };
     let sink = Sink::try_new(&stream_handle)?;
 
     loop {
         match handle_events(&mut server, &sink, &args) {
             Err(e) if e.to_string() == "Connection closed normally" => {
                 warn!("Lost connection to server, attempting to reconnect");
-                server = connect_to_server(&args.url, &args.username, args.delay, args.backoff)?;
+                server = match connect_to_server(&args.url, &args.username, args.delay, args.backoff, args.attempts) {
+                    Ok(server) => server,
+                    Err(e) => {
+                        error!("{}", e);
+                        return Ok(());
+                    },
+                };
             },
             Err(e) => error!("Error: {}", e),
             _ => (),
@@ -60,15 +79,22 @@ fn main() -> Result<()> {
     }
 }
 
-fn connect_to_server(url: &str, username: &str, delay: u64, backoff: u64) -> Result<WebSocket<MaybeTlsStream<TcpStream>>> {
-    let mut wait = delay;
+fn connect_to_server(url: &str, username: &str, delay: u64, backoff: u64, attempts: u64) -> Result<WebSocket<MaybeTlsStream<TcpStream>>> {
+    let mut delay = delay;
+    let mut retries = 1;
     let (mut server, _) = loop {
-        if let Ok(server) = connect(url) {
-            break server;
+        match connect(url) {
+            Ok(server) => break server,
+            Err(_e) => {
+                if retries > attempts {
+                    return Err(anyhow!("Attempts exhausted, exiting"));
+                }
+                warn!("Failed to connect to server {}, retrying in {} seconds. {}/{}", url, delay, retries, attempts);
+                std::thread::sleep(std::time::Duration::from_secs(delay));
+                delay *= backoff;
+                retries += 1;
+            }
         }
-        warn!("Failed to connect to server {}, retrying in {} seconds", url, wait);
-        std::thread::sleep(std::time::Duration::from_secs(wait));
-        wait *= backoff;
     };
     server.send(Message::Text(username.into()))
         .with_context(|| format!("Failed to connect to server {}", url))?;
@@ -81,8 +107,8 @@ fn handle_events(server: &mut WebSocket<MaybeTlsStream<TcpStream>>, sink: &Sink,
     let response_as_text = response.to_text()?;
     let msg: Msg = serde_json::from_str(response_as_text)
         .with_context(|| format!("Server sent invalid response {}", response_as_text))?;
-    let msg_type = &msg.msg_type.as_str();
-    match *msg_type {
+    let msg_type = msg.msg_type.as_str();
+    match msg_type {
         "bgm" => play_audio(msg, &sink)?,
         "gameStatus" => handle_status(msg, &sink, &args)?,
         _ => error!("Server sent invalid msgType {}", msg_type),
@@ -94,7 +120,7 @@ fn play_audio(msg: Msg, sink: &Sink) -> Result<()> {
     sink.stop();
     let url = msg.audio_url
         .context("Server sent response of type bgm but no URL was provided")?;
-    let audio = get(&url)
+    let audio = reqwest::blocking::get(&url)
         .with_context(|| format!("URL {} is invalid", url))?;
     let cursor = Cursor::new(audio.bytes()?);
     let source = Decoder::new(cursor)?;
