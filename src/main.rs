78098c0f183d6063b167d7b72823e09ffd2180e4
someone <40119497+some100@users.noreply.github.com>
2025-02-24 12:47:49 +0000

    consistency + deserialize

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::io::Cursor;
 use std::net::TcpStream;
-use std::time::Duration;
-use std::thread::sleep;
 use clap::Parser;
 use rodio::{Sink, Decoder, OutputStream};
-use serde_json::Value;
+use serde::Deserialize;
 use tungstenite::{WebSocket, connect, Message};
 use tungstenite::stream::MaybeTlsStream;
 use reqwest::blocking::get;
-use anyhow::{Context, anyhow, Result};
+use anyhow::{Context, Result};
 use log::{debug, info, warn, error};
 use env_logger::Env;
 
@@ -25,11 +23,19 @@ struct Args {
     #[arg(short, long, default_value = "ws://client.fe2.io:8081")]
     url: String,
     /// Delay for failed connection in seconds
-    #[arg(long, default_value_t = 1)]
+    #[arg(long, default_value_t = 5)]
     delay: u64,
     /// Multiplier for delay in failed connection
     #[arg(long, default_value_t = 2)]
     backoff: u64,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Msg {
+    msg_type: String,
+    audio_url: Option<String>,
+    status_type: Option<String>,
 }
 
 fn main() -> Result<()> {
@@ -48,7 +54,7 @@ fn main() -> Result<()> {
                 warn!("Lost connection to server, attempting to reconnect");
                 server = connect_to_server(&args.url, &args.username, args.delay, args.backoff)?;
             },
-            Err(e) => error!("Error: {e}"),
+            Err(e) => error!("Error: {}", e),
             _ => (),
         }
     }
@@ -60,54 +66,51 @@ fn connect_to_server(url: &str, username: &str, delay: u64, backoff: u64) -> Res
         if let Ok(server) = connect(url) {
             break server;
         }
-        warn!("Failed to connect to server {url}, retrying in {wait} seconds");
-        sleep(Duration::from_secs(wait));
+        warn!("Failed to connect to server {}, retrying in {} seconds", url, wait);
+        std::thread::sleep(std::time::Duration::from_secs(wait));
         wait *= backoff;
     };
     server.send(Message::Text(username.into()))
         .with_context(|| format!("Failed to connect to server {}", url))?;
-    info!("Connected to server {url} with username {username}");
+    info!("Connected to server {} with username {}", url, username);
     Ok(server)
 }
 
 fn handle_events(server: &mut WebSocket<MaybeTlsStream<TcpStream>>, sink: &Sink, args: &Args) -> Result<()> {
     let response = server.read()?;
     let response_as_text = response.to_text()?;
-    let msg: Value = serde_json::from_str(response_as_text)
-        .with_context(|| format!("Server sent back invalid response {}", response_as_text))?;
-    let msg_type = &msg["msgType"].as_str()
-        .ok_or_else(|| anyhow!("Server sent back response without msgType"))?;
+    let msg: Msg = serde_json::from_str(response_as_text)
+        .with_context(|| format!("Server sent invalid response {}", response_as_text))?;
+    let msg_type = &msg.msg_type.as_str();
     match *msg_type {
         "bgm" => play_audio(msg, &sink)?,
         "gameStatus" => handle_status(msg, &sink, &args)?,
-        _ => error!("Server sent invalid msgType {}", *msg_type),
+        _ => error!("Server sent invalid msgType {}", msg_type),
     };
     Ok(())
 }
 
-fn play_audio(msg: Value, sink: &Sink) -> Result<()> {
+fn play_audio(msg: Msg, sink: &Sink) -> Result<()> {
     sink.stop();
-    let url = msg["audioUrl"]
-        .as_str()
-        .ok_or_else(|| anyhow!("Server sent response of type bgm but no URL was provided"))?;
-    let audio = get(url)
+    let url = msg.audio_url
+        .context("Server sent response of type bgm but no URL was provided")?;
+    let audio = get(&url)
         .with_context(|| format!("URL {} is invalid", url))?;
     let cursor = Cursor::new(audio.bytes()?);
     let source = Decoder::new(cursor)?;
     sink.append(source);
-    debug!("Playing audio {url}");
+    debug!("Playing audio {}", url);
     Ok(())
 }
 
-fn handle_status(msg: Value, sink: &Sink, args: &Args) -> Result<()> {
-    let statustype = msg["statusType"]
-        .as_str()
-        .ok_or_else(|| anyhow!("Server sent response of type gameStatus but no status was provided"))?;
-    match statustype {
+fn handle_status(msg: Msg, sink: &Sink, args: &Args) -> Result<()> {
+    let statustype = msg.status_type
+        .context("Server sent response of type gameStatus but no status was provided")?;
+    match statustype.as_str() {
         "died" => sink.set_volume(args.volume),
         "left" => sink.clear(),
         _ => (),
     }
-    debug!("Set game status to {statustype}");
+    debug!("Set game status to {}", statustype);
     Ok(())
 }
