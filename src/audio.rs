e5ba91f82e425fd1582ae2a530f2f01cba4249c6
someone <ootinnyoo@outlook.com>
2025-03-06 09:44:35 -0500

    add time out for http reqs

--- src/audio.rs
+++ src/audio.rs
@@ -1,12 +1,17 @@
 use crate::{Fe2IoError, Args};
 use std::io::Cursor;
-use tokio::sync::mpsc::Receiver;
+use tokio::{
+    time::Duration,
+    sync::mpsc::Receiver,
+};
 use rodio::{Sink, Decoder};
 use reqwest::Client;
 use log::error;
 
 pub async fn audio_loop(sink: Sink, mut rx: Receiver<String>, args: Args) -> Result<(), Fe2IoError> {
-    let client = Client::new();
+    let client = Client::builder()
+        .connect_timeout(Duration::from_secs(5))
+        .build()?;
     loop {
         match handle_audio_inputs(&sink, &mut rx, &client, &args).await {
             Err(Fe2IoError::RecvClosed()) => {
@@ -33,12 +38,14 @@ async fn handle_audio_inputs(sink: &Sink, rx: &mut Receiver<String>, client: &Cl
 async fn play_audio(sink: &Sink, client: &Client, input: &str) -> Result<(), Fe2IoError> {
     sink.set_volume(1.0); // Volume is set to 1.0 by default. If this is too low or too high, you can manually change your volume
     sink.stop();
-    let audio = client.get(input)
+    let response = client
+        .get(input)
         .send()
         .await?;
-    if !audio.status().is_success() {
-        return Err(Fe2IoError::Invalid(format!("URL {} returned error status {}", input, audio.status())));
-    }
+    let audio = match response.error_for_status() {
+        Ok(audio) => audio,
+        Err(e) => return Err(Fe2IoError::Http(e)),
+    };
     let cursor = Cursor::new(audio.bytes().await?);
     let source = Decoder::new(cursor)?;
     sink.append(source);
